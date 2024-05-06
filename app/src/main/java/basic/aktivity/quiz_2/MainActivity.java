dfdc19840677c01ad8571dd4d47c4d4f072b4d7c
widya maulida <widyamaulida>
2024-05-06 15:39:18 +0700

    Initial commit

---
+++ app/src/main/java/basic/aktivity/quiz_2/MainActivity.java
@@ -0,0 +1,34 @@
+package basic.aktivity.quiz_2;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText etPorsi;
+    RadioButton rbNasgor, rbMiegor, RbMiereb, rbNasduk;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        etPorsi = findViewById(R.id.etPorsi);
+        rbNasgor =findViewById(R.id.rbNasgor);
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
